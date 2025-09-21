f050f6cc2de55f523490426b9393895afa363c7c
ceciliaMastrocola <ceciliamastrocola@gmail.com>
2025-09-21 01:56:30 -0300

    ADD_MetodoDadosCargados_JugadorCasino

--- IE_INTERFAZ_GRAFICA/src/ie_interfaz_grafica/JugadorCasino.java
+++ IE_INTERFAZ_GRAFICA/src/ie_interfaz_grafica/JugadorCasino.java
@@ -3,26 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ie_interfaz_grafica;
-
+import java.util.Random; 
 /**
  * Atributos Clase Jugador
  * private String nombre;
     private int dinero;
     private int partidasGanadas;
 
-    // Constructor
-    public Jugador(String nombre, int dineroInicial) {
-        this.nombre = nombre;
-        this.dinero = Math.max(0, dineroInicial); 
-        this.partidasGanadas = 0;
-    }
+   
  * @author Ceciia
  */
 // CONSIGNA 3: JUGADOR CASINO
 public class JugadorCasino extends Jugador{
+      
+    private final Random random;
      //Constructor:
         public JugadorCasino(String nombre, int dineroInicial) {
         super("Casino", dineroInicial); // Nombre fijo "Casino"
+        this.random= new Random();
     }
    
 // Implementación del método abstracto para obtener el tipo de jugador
@@ -34,7 +32,24 @@ public class JugadorCasino extends Jugador{
     // Implementación del método abstracto para calcular la apuesta
         @Override   
         public int calcularApuesta() {
-        int apuesta = (int)(getDinero() * 0.4);
-        return Math.max(apuesta, 1);
+        return 0; // El casino no apuesta dinero
+    }
+        
+        //Habilidad especial: dados cargados (40% probabilidad de sacar 6 en cada dado)
+    public int tirarDadoCargado() {
+        if (random.nextDouble() < 0.4) {  
+            return 6; // 40% de probabilidad
+        } else {
+            return random.nextInt(6) + 1; // valor normal 1–6
+        }
+    }
+
+    // Método para lanzar dos dados cargados
+    public int lanzarDadosCargados() {
+        int dado1 = tirarDadoCargado();
+        int dado2 = tirarDadoCargado();
+        System.out.println("Casino tiró dados: " + dado1 + " + " + dado2 + " = " + (dado1 + dado2));
+        return dado1 + dado2;
     }
 }
+
